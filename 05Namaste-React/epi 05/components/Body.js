fb982815194529a37f35616d0410bb8c1f35eeda
kartikshingde <kartikshingde12@gmail.com>
2025-02-26 18:42:02 +0530

    search functionality,api,stateVariable

---
+++ 05Namaste-React/epi 05/components/Body.js
@@ -0,0 +1,112 @@
+import RestarauntCard from "./RestaurantCard";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
+
+const Body = () => {
+  //Local State Variable
+  const [listOfRestaurants, setlistOfRestaurants] = useState([]);
+
+  const [filteredRestaurant, setfilteredRestaurant] = useState("");
+
+  const [searchText, setsearchText] = useState("");
+
+  //WhenEver state variable update, react trigger Reconcilation cycle(i.e. re-renders component)***
+  console.log("Body Rendered");
+
+  //useEffect()
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    console.log("Fetched");
+    // console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+
+    //optional Chaining
+    setlistOfRestaurants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+
+    setfilteredRestaurant(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  //Conditional Rendering -> rendering on the basis of condition
+  // if(listOfRestaurants.length===0){
+  //   return <Shimmer/>
+  // }
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            placeholder="search restaurant"
+            value={searchText}
+            onChange={(e) => {
+              setsearchText(e.target.value);
+            }}
+          />
+
+          <button
+            onClick={() => {
+              //filter the res Cards and update the UI
+              //need searchText
+              console.log(searchText);
+
+              const filteredRestaurant = listOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+
+              setfilteredRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
+
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating >= 4.5
+            );
+            setfilteredRestaurant(filteredList);
+            console.log(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+
+        <button
+          className="filter-btn"
+          onClick={() => {
+            setfilteredRestaurant(listOfRestaurants);
+          }}
+        >
+          View All Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {/* looping through the <RestaurentCard /> components Using Array.map() method */}
+
+        {filteredRestaurant.map((restaurant) => (
+          <RestarauntCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
---
+++ 05Namaste-React/epi 05/components/Header.js
@@ -0,0 +1,36 @@
+import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
+export const Header = () => {
+  let btnName = "Login"; //Normal JS variable
+  const [btnNameReact, setbtnNameReact] = useState("Login");
+
+  console.log("Header Rendered");
+
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnNameReact === "Login"
+                ? setbtnNameReact("LogOut")
+                : setbtnNameReact("Login");
+            }}
+          >
+            {btnNameReact}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+// export default Header;
