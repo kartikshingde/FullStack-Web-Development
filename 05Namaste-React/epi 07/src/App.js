a6aca9eae6b67d0b76723e8d869dc13e2da954b8
kartikshingde <kartikshingde12@gmail.com>
2025-02-26 20:22:11 +0530

    error in router v7

---
+++ 05Namaste-React/epi 07/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Header } from "./components/Header";
+//{Header} beacause it is named import 
+import Body from "./components/Body";
+import {createBrowserRouter} from "react-router-dom"
+
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
