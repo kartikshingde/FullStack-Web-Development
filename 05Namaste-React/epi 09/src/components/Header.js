d7ffe7e19f1a8f5d6f2164e9f4088f291ab26d11
kartikshingde <kartikshingde12@gmail.com>
2025-03-18 20:24:38 +0530

    optimization ep 09,custom hooks,chunking-lazy(),response()

---
+++ 05Namaste-React/epi 09/src/components/Header.js
@@ -0,0 +1,56 @@
+import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
+import { Link } from "react-router";
+import useOnlineStatus from "../utils/useOnlineStatus";
+
+export const Header = () => {
+  const [btnNameReact, setbtnNameReact] = useState("Login");
+
+  // console.log("Header Rendered");
+
+  // 1 .if no dependency array then useEffect() is called on every render
+  // 2 .if dependency array is empty then useEffect is called Initial render (just Once)
+  // 3 .if dependency array has btnNameReact then it is called whenever btnNameReact changes...
+
+  const onlineStatus = useOnlineStatus();
+
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Online Status: {onlineStatus ? "✅" : "⛔"}</li>
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/about"}>About Us</Link>
+          </li>
+          <li>
+            <Link to={"/contact"}>Contact Us</Link>
+          </li>
+          <li>
+            <Link to="/grocery" className="links">
+              Grocery
+            </Link>
+          </li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnNameReact === "Login"
+                ? setbtnNameReact("LogOut")
+                : setbtnNameReact("Login");
+            }}
+          >
+            {btnNameReact}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+// export default Header;
---
+++ 05Namaste-React/epi 09/src/components/RestaurantMenu.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
+import { useParams } from "react-router";
+import { MENU_API } from "../utils/constants";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
+
+const RestaurantMenu = () => {
+  const { resId } = useParams();
+
+  const resInfo = useRestaurantMenu(resId);
+
+  if (resInfo === null) return <h1>Loading...</h1>;
+
+  if (resInfo === null) return <Shimmer />;
+  // not used in ternary operator because, we have use here before destructuring as we set resInfo = null Initially
+  // so it cannot destructure name,... of null
+
+  const { name, cuisines, costForTwoMessage } =
+    resInfo?.cards[2]?.card?.card?.info;
+
+  const { itemCards } =
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+  console.log(itemCards);
+
+  return (
+    <div className="menu">
+      <div className="infoMenu">
+        <h1 className="resName">{name}</h1>
+        <h4>{cuisines.join(" ,")}</h4>
+        <h4>{costForTwoMessage}</h4>
+        <h2>Menu</h2>
+        <ul>
+          {itemCards.map((item) => (
+            <li key={item.card.info.id}>
+              {" "}
+              {item.card.info.name}{" "}
+              {<span>{`- Rs.  ` + item.card.info.price / 100}</span> ||
+                `- Rs.  ` + item.card.info.defaultPrice / 100}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantMenu;
