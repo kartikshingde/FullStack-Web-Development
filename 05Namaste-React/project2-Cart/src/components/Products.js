78f4b2891b64d559f37185ca97c6e9ba5455ca88
kartikshingde <kartikshingde12@gmail.com>
2025-04-09 10:52:33 +0530

    .

---
+++ 05Namaste-React/project2-Cart/src/components/Products.js
@@ -0,0 +1,9 @@
+
+import React from 'react'
+
+export const products=[
+    {id:1,name:"apple",price:250},
+    {id:2,name:"banana",price:150},
+    {id:1,name:"straberry",price:450},
+
+]
---
+++ 05Namaste-React/project2-Cart/src/context/CartContext.js
@@ -0,0 +1,11 @@
+import { createContext } from "react";
+
+const CartContext = createContext();
+
+// export const CartProvider = ({ children }) => {
+//   const [cartItems,setCartItems]=useState([])
+//   const addToCart=(products)=>{
+//     const exists=
+//     setCartItems(...cartItems,products)
+//   }
+// };
